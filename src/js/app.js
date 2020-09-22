93a3eaa5d37985d81cffda2f9feff7d44976d636
Valerie Sidman <valerie.sidman@gmail.com>
2020-09-22 14:05:16 +0300

    bugs fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,19 @@
 export default function sorting(characters) {
-    return characters.sort((a, b) => {
-        if (a.health < b.health) {
-            return 1;
-        } else if (a.health > b.health) {
-            return -1;
-        } else {
-            if (a.name < b.name) {
-                return -1;
-            } else if (a.name > b.name) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    })
+  return characters.sort((a, b) => {
+    if (a.health < b.health) {
+      return 1;
+    }
+
+    if (a.health > b.health) {
+      return -1;
+    }
+
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
 }
